220df713d44d69d5ff4c33549b9ffa358f0dc151
Serif <145980658+mserifc@users.noreply.github.com>
2024-11-30 15:08:25 +0300

    Update display.h

--- include/display.h
+++ include/display.h
@@ -1,54 +1,40 @@
-#ifndef DISPLAY_H
-#define DISPLAY_H
+#pragma once
 
 #include "types.h"
+#include "port.h"
 
-#define DisplayWidth 80
-#define DisplayHeight 25
+// Define screen width and height for the Command Line Interface (CLI)
+#define display_CLIWIDTH 80     // 80 columns
+#define display_CLIHEIGHT 25    // 25 rows
 
-enum DisplayColorTable {
-    COLOR_BLACK = 0x00,
-    COLOR_BLUE = 0x01,
-    COLOR_GREEN = 0x02,
-    COLOR_CYAN = 0x03,
-    COLOR_RED = 0x04,
-    COLOR_MAGENTA = 0x05,
-    COLOR_BROWN = 0x06,
-    COLOR_GRAY = 0x07,
-    COLOR_DARK_GRAY = 0x08,
-    COLOR_LIGHT_BLUE = 0x09,
-    COLOR_LIGHT_GREEN = 0x0A,
-    COLOR_LIGHT_CYAN = 0x0B,
-    COLOR_LIGHT_RED = 0x0C,
-    COLOR_LIGHT_MAGENTA = 0x0D,
-    COLOR_YELLOW = 0x0E,
-    COLOR_WHITE = 0x0F
+// Enumeration for display color
+enum display_COLORTABLE {
+    display_COLOR_BLACK         = 0x00,
+    display_COLOR_BLUE          = 0x01,
+    display_COLOR_GREEN         = 0x02,
+    display_COLOR_CYAN          = 0x03,
+    display_COLOR_RED           = 0x04,
+    display_COLOR_MAGENTA       = 0x05,
+    display_COLOR_BROWN         = 0x06,
+    display_COLOR_GRAY          = 0x07,
+    display_COLOR_DARK_GRAY     = 0x08,
+    display_COLOR_LIGHT_BLUE    = 0x09,
+    display_COLOR_LIGHT_GREEN   = 0x0A,
+    display_COLOR_LIGHT_CYAN    = 0x0B,
+    display_COLOR_LIGHT_RED     = 0x0C,
+    display_COLOR_LIGHT_MAGENTA = 0x0D,
+    display_COLOR_YELLOW        = 0x0E,
+    display_COLOR_WHITE         = 0x0F
 };
 
-typedef enum DisplayColorTable Color;
+// Enum type alias for display color, to use as a color reference
+typedef enum display_COLORTABLE display_Color;
 
-typedef struct {
-    void (*clear)();
-    void (*reload)();
-    int (*putchar)(char chr, int point);
-    char (*getchar)(int point);
-    int (*loadcursor)(int point);
-    void (*setTextColor)(Color color);
-    void (*setBackgroundColor)(Color color);
-    Color (*getTextColor)();
-    Color (*getBackgroundColor)();
-} Display;
-
-void display_clear();
-void display_reload();
-
-int display_putchar(char chr, int point);
-char display_getchar(int point);
-int display_loadcursor(int point);
-
-void display_setTextColor(Color color);
-void display_setBackgroundColor(Color color);
-Color display_getTextColor();
-Color display_getBackgroundColor();
-
-#endif // DISPLAY_H
+// Function prototypes
+void display_clear();                                   // Clear the entire screen
+void display_putchar(char chr, int ptr);                // Print a single character at a specified position (pointer) on the screen
+char display_getchar(int ptr);                          // Get the character at a specific screen position (pointer)
+void display_enablecursor(uint8_t start, uint8_t end);  // Enable the cursor, specifying the cursor size (not position)
+void display_disablecursor();                           // Disable the cursor (hide it from the screen)
+void display_putcursor(int ptr);                        // Set the cursor to a specific screen position (pointer)
+size_t display_getcursor();                             // Get the current position of the cursor
