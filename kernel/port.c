a902700cefd9c5b56445fcb63b53cc8b907f07b1
Serif <145980658+mserifc@users.noreply.github.com>
2024-11-30 15:07:49 +0300

    Update port.c

--- kernel/port.c
+++ kernel/port.c
@@ -1,5 +1,6 @@
 #include "port.h"
 
+// Port Input Function (inb)
 uint8_t port_inb(uint16_t port) {
     uint8_t value;
     asm volatile (
@@ -10,10 +11,16 @@ uint8_t port_inb(uint16_t port) {
     return value;
 }
 
+// Port Output Function (outb)
 void port_outb(uint16_t port, uint8_t value) {
     asm volatile (
         "outb %0, %1"
         :
         : "a"(value), "Nd"(port)
     );
+}
+
+// Port I/O Wait Function
+void port_ioWait(void) {
+    asm volatile ("outb %%al, $0x80" : : "a"(0));
 }
