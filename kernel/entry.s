897e908cc568247b1ac31a3b0b121a5c5c6629a8
Serif <145980658+mserifc@users.noreply.github.com>
2024-11-30 15:06:07 +0300

    Update entry.s

--- kernel/entry.s
+++ kernel/entry.s
@@ -1,27 +1,27 @@
-.set MAGIC, 0x1BADB002
-.set FLAGS, (1<<0 | 1<<1)
-.set CHECKSUM, -(MAGIC + FLAGS)
+.set MAGIC, 0x1BADB002              # Multiboot Magic Number
+.set FLAGS, (1<<0 | 1<<1)           # Multiboot Flags (Send Memory Info and Boot Dervice)
+.set CHECKSUM, -(MAGIC + FLAGS)     # Multiboot Checksum for Verify Header
 
-.section .multiboot
-    .long MAGIC
-    .long FLAGS
-    .long CHECKSUM
+.section .multiboot         # Multiboot Header
+    .long MAGIC             # Set Magic Number
+    .long FLAGS             # Set Flags
+    .long CHECKSUM          # Set Checksum
 
-.section .text
-    .extern _kernel_init
-    .global _kernel_entry
+.section .text              # Code Section
+    .extern kernel_init     # Include Kernel Initialize Function
+    .global kernel_entry    # Make Kernel Entry Global Symbol
 
-_kernel_entry:
-    mov $_kernel_stack, %esp
-    push %eax
-    push %ebx
-    call _kernel_init
+kernel_entry:                   # Entry of Kernel
+    mov $kernel_stack, %esp     # Set Kernel Stack as Stack Pointer
+    push %eax                   # Save EAX Register
+    push %ebx                   # Save EBX Register
+    call kernel_init            # Initialize Kernel
 
-_halt:
-    cli
-    hlt
-    jmp _halt
+halt:           # Halt the CPU if Failed
+    cli         # Disable Interrupts
+    hlt         # Halt the CPU
+    jmp halt    # If CPU escapes, bring to top of Halt Function
 
-.section .bss
-    .space 2*1024*1024
-_kernel_stack:
+.section .bss               # BSS Section
+    .space 2*1024*1024      # Reserve 2MB
+kernel_stack:               # Set Kernel Stack Symbol
