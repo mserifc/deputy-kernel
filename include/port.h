f09a056814143c3ba228c7fde01cf46efc4fcb68
Serif <145980658+mserifc@users.noreply.github.com>
2025-01-14 16:02:09 +0300

    Delete include directory

--- include/port.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "types.h"
-
-// Function Prototypes
-uint8_t port_inb(uint16_t port);                    // Set Port Input Function (inb)
-void port_outb(uint16_t port, uint8_t value);       // Set Port Output Function (outb)
-void port_ioWait(void);                             // Set I/O Wait Function
